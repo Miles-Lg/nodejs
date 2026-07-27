60f3be4cf4653dd42994743be40cbc16f5a05204
Emilio LG <milio.code@gmail.com>
2026-07-27 20:30:16 +0100

    added dependencies

--- npm/index.js
+++ npm/index.js
@@ -1,5 +1,11 @@
-const generateName = require("sillyname")
+import generateName from "sillyname"
 
 const sillyName = generateName()
-
 console.log(`My name is: ${sillyName}`)
+
+
+
+import superHeroes from "superheroes"
+
+const heroes = superHeroes.random()
+console.log(`I'm ${heroes}`)
