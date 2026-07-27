2293a7512c6574609b36759d87cb04f7b8aa5c07
Emilio LG <milio.code@gmail.com>
2026-07-27 21:23:10 +0100

    updated files

---
+++ npm/QR-Code-project/index.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer"
+import qr from "qr-image"
+import fs from "fs"
+import { type } from "os"
+
+inquirer
+  .prompt([
+    {
+      message: "Type ing your URL: ",
+      name: "URL"
+    }
+  ])
+  .then(answer => {
+    const url = answer.URL
+    const qr_img = qr.image(url)
+    qr_img.pipe(fs.createWriteStream("qr_img.png"))
+
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err
+      console.log("The file has been saved")
+    })
+  })
+  .catch(error => {
+    if (error.isTtyError) {
+      // prompt couldn't be rendered int he current environment
+    } else {
+      // Something else went wrong
+    }
+  })
