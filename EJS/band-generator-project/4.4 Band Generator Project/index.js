73f94f87d85f855cd095321ad958e3c71b13e4db
Emilio LG <milio.code@gmail.com>
2026-08-09 00:13:42 +0100

    updated

--- EJS/band-generator-project/4.4 Band Generator Project/index.js
+++ EJS/band-generator-project/4.4 Band Generator Project/index.js
@@ -16,13 +16,13 @@ app.use(express.static("public"))
 
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
-const year = new Date().getFullYear()
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
-  res.render("index.ejs", { currentYear: year })
+  res.render("index.ejs")
 });
 
 app.post("/submit", (req, res) => {
